e3fbf177506ccbd18331f79c35e2b1b6e70ba1fd
ChrisColind <cristopher_rogelio@unitec.edu>
2026-03-02 08:44:07 -0600

    primer commit

---
+++ src/proyecto_insta/Proyecto_Insta.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package proyecto_insta;
+
+/**
+ *
+ * @author croge
+ */
+public class Proyecto_Insta {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("aaaa");
+        
+        
+            
+    }
+    
+}
