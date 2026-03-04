9b5e14ba66ad2244c5f3f3210c5fe0140a0a85ec
ChrisColind <cristopher_rogelio@unitec.edu>
2026-03-04 08:58:24 -0600

    avance 4 interfaces, enums

---
+++ src/proyecto_insta/GestionUser.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package proyecto_insta;
+
+/**
+ *
+ * @author croge
+ */
+
+//para gestionar datos de los usuarios
+interface GestionUser{
+    void GuardarCuenta() throws Exception;
+    void Cargar() throws Exception;
+    void Eliminar() throws Exception;
+
+}
